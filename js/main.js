92b2fb770fbf48b5ebbbfd6f51197c7ce3271b20
andriimakukha2 <andriimakukha2.0@gmail.com>
2024-09-06 09:57:03 +0200

    fixed code

--- js/main.js
+++ js/main.js
@@ -1,18 +1,14 @@
 function isValidEmail(email) {
-    // Регулярний вираз для перевірки валідності електронної адреси
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    // Перевірка рядка на відповідність регулярному виразу
     return emailPattern.test(email);
 }
 
 // #2
 
 function isValidUrl(url) {
-    // Регулярний вираз для перевірки валідності URL
     const urlPattern = /^(https?:\/\/)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(:\d+)?(\/[^\s]*)?$/;
 
-    // Перевірка рядка на відповідність регулярному виразу
     return urlPattern.test(url);
 }
 
